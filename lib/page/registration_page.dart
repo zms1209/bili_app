32cca647638b84e24b4d8010ccef501f4cf73b71
andy <andy@126.com>
2025-09-11 17:18:39 +0800

    feat: 4-7 注册模块设计与实现

--- lib/page/registration_page.dart
+++ lib/page/registration_page.dart
@@ -1,10 +1,16 @@
+import 'package:bili_app/http/core/hi_error.dart';
+import 'package:bili_app/http/dao/login_dao.dart';
+import 'package:bili_app/util/string_util.dart';
+import 'package:bili_app/util/toast.dart';
 import 'package:bili_app/widget/appbar.dart';
 import 'package:bili_app/widget/login_effect.dart';
 import 'package:bili_app/widget/login_input.dart';
 import 'package:flutter/material.dart';
 
 class RegistrationPage extends StatefulWidget {
-  const RegistrationPage({super.key});
+  final VoidCallback? onJumpToLogin;
+
+  const RegistrationPage({super.key, this.onJumpToLogin});
 
   @override
   State<RegistrationPage> createState() => _RegistrationPageState();
@@ -12,6 +18,12 @@ class RegistrationPage extends StatefulWidget {
 
 class _RegistrationPageState extends State<RegistrationPage> {
   bool protect = false;
+  bool loginEnable = false;
+  String? userName;
+  String? password;
+  String? rePassword;
+  String? imoocId;
+  String? orderId;
 
   @override
   Widget build(BuildContext context) {
@@ -24,30 +36,130 @@ class _RegistrationPageState extends State<RegistrationPage> {
         child: ListView(
           children: [
             LoginEffect(protect: protect,),
+            LoginEffect(
+              protect: protect,
+            ),
             LoginInput(
-              '用户名',
-              '请输入用户名',
+              "用户名",
+              "请输入用户名",
               onChanged: (text) {
-                print(text);
+                userName = text;
+                checkInput();
               },
             ),
             LoginInput(
-              '密码',
-              '请输入密码',
+              "密码",
+              "请输入密码",
               obscureText: true,
-              lineStretch: true,
               onChanged: (text) {
-                print(text);
+                password = text;
+                checkInput();
               },
               focusChanged: (focus) {
-                setState(() {
+                this.setState(() {
                   protect = focus;
                 });
               },
+            ),
+            LoginInput(
+              "确认密码",
+              "请再次输入密码",
+              lineStretch: true,
+              obscureText: true,
+              onChanged: (text) {
+                rePassword = text;
+                checkInput();
+              },
+              focusChanged: (focus) {
+                this.setState(() {
+                  protect = focus;
+                });
+              },
+            ),
+            LoginInput(
+              "慕课网ID",
+              "请输入你的慕课网用户ID",
+              keyboardType: TextInputType.number,
+              onChanged: (text) {
+                imoocId = text;
+                checkInput();
+              },
+            ),
+            LoginInput(
+              "课程订单号",
+              "请输入课程订单号后四位",
+              keyboardType: TextInputType.number,
+              lineStretch: true,
+              onChanged: (text) {
+                orderId = text;
+                checkInput();
+              },
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+              child: LoginButton(
+                '注册',
+                enable: loginEnable,
+                onPressed: checkParams
+              ),
             )
           ],
         ),
       ),
     );
+  }
+
+  void checkInput() {
+    bool enable;
+    if (isNotEmpty(userName) &&
+        isNotEmpty(password) &&
+        isNotEmpty(rePassword) &&
+        isNotEmpty(imoocId) &&
+        isNotEmpty(orderId)) {
+      enable = true;
+    } else {
+      enable = false;
+    }
+    setState(() {
+      loginEnable = enable;
+    });
+  }
+
+  void checkParams() {
+    String? tips;
+    if (password != rePassword) {
+      tips = '两次密码不一致';
+    } else if (orderId?.length != 4) {
+      tips = "请输入订单号的后四位";
+    }
+    if (tips != null) {
+      print(tips);
+      return;
+    }
+    send();
+  }
+
+  void send() async {
+    try {
+      var result =
+      await LoginDao.registration(userName!, password!, imoocId!, orderId!);
+      print(result);
+      if (result['code'] == 0) {
+        print('注册成功');
+        showToast('注册成功');
+        if (widget.onJumpToLogin != null) {
+          widget.onJumpToLogin!();
+        }
+      } else {
+        print(result['msg']);
+        showWarnToast(result['msg']);
+      }
+    } on NeedAuth catch (e) {
+      print(e);
+      showWarnToast(e.message);
+    } on HiNetError catch (e) {
+      print(e);
+      showWarnToast(e.message);
+    }
   }
 }
